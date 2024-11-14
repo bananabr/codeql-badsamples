7cb910ecc98bb1660040a4c4732783bfb1ac71a3
Daniel Santos <bananabr@users.noreply.github.com>
2024-11-14 11:32:20 -0600

    dom-cloberring case

---
+++ js/dom-cloberring/dom-clob.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    let someObject = window.someObject || {};
+    let script = document.createElement('script');
+    script.src = someObject.url;
+    document.body.appendChild(script);
+};
